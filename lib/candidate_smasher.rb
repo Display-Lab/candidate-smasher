6a64ec70f0b668a3327a14d05d2053a1e8b172a5
Colin Gross <grosscol@gmail.com>
2018-05-31 16:46:15 -0400

    Implemente candidate generation Move fixutures to spec in prep for end to end testing.

--- lib/candidate_smasher.rb
+++ lib/candidate_smasher.rb
@@ -1,12 +1,17 @@
 require "json/ld"
+require "digest"
 
 class CandidateSmasher
   SPEK_IRI = "http://purl.obolibrary.org/obo/fio#SPEK"
   HAS_PERFORMER_IRI= "http://purl.obolibrary.org/obo/fio#HasPerformer"
   USES_TEMPLATE_IRI = "http://purl.obolibrary.org/obo/fio#UsesTemplate"
   USES_ISR_IRI = "http://purl.obolibrary.org/obo/fio#UsesISR"
+  ANCESTOR_PERFORMER_IRI = "http://purl.obolibrary.org/obo/fio#AncestorPerformer"
+  ANCESTOR_TEMPLATE_IRI = "http://purl.obolibrary.org/obo/fio#AncestorTemplate"
+  CANDIDATE_IRI = "http://purl.obolibrary.org/obo/fio#AncestorTemplate"
+  HAS_CANDIDATE_IRI = "http://purl.obolibrary.org/obo/fio#hasCandidate"
 
-  attr_reader :spek_hsh
+  attr_accessor :spek_hsh
   
   def initialize(input_string="{}")
     begin
@@ -24,7 +29,33 @@ class CandidateSmasher
     checks.all?{|c| c}
   end
 
-  def generate_candiates
+  def generate_candidates
+    performers = @spek_hsh[HAS_PERFORMER_IRI]
+    templates = @spek_hsh[USES_TEMPLATE_IRI]
 
+    res = performers.collect do |p|
+      templates.collect{|t| CandidateSmasher.make_candidate(t,p) }
+    end
+
+    res.flatten
+  end
+
+  def smash!
+    candidates = generate_candidates
+    @spek_hsh[HAS_CANDIDATE_IRI] = candidates
+    JSON.dump(@spek_hsh)
+  end
+
+  def self.make_candidate(template, performer)
+    t_id = template["@id"]
+    p_id = performer["@id"]
+
+    candidate = template.merge performer
+    candidate["@type"] = CANDIDATE_IRI
+    candidate["@id"] = "candidate.internal/" + Digest::MD5.hexdigest(t_id + p_id)
+    candidate[ANCESTOR_PERFORMER_IRI] = p_id
+    candidate[ANCESTOR_TEMPLATE_IRI]  = t_id
+
+    return(candidate)
   end
 end
--- spec/candidate_smasher_spec.rb
+++ spec/candidate_smasher_spec.rb
@@ -1,54 +1,127 @@
+require 'json'
 require './lib/candidate_smasher'
 
 RSpec.describe CandidateSmasher do
 
-  describe "#valid?" do
-
-    # a la carte json components of spek
-    let(:type)  {'"@type": "http://purl.obolibrary.org/obo/fio#SPEK"'}
-    let(:bad_type)  {'"@type": "http://example.com/not/a/spek"'}
-    let(:attr1) {'"http://purl.obolibrary.org/obo/fio#HasPerformer": []'}
-    let(:attr2) {'"http://purl.obolibrary.org/obo/fio#UsesTemplate": []'}
-    let(:attr3) {'"http://purl.obolibrary.org/obo/fio#UsesISR": []'}
-
-    let(:full)    { "{#{type}, #{attr1}, #{attr2}, #{attr3}}" }
-    let(:no_type) { "{#{attr1}, #{attr2}, #{attr3}}" }
-    let(:not_spek)    { "{#{bad_type}, #{attr1}, #{attr2}, #{attr3}}" }
-    let(:missing_attr)    { "{#{type}, #{attr1}, #{attr3}}" }
-
-    it "is invalid when content is invalid json" do
+  describe "#initialize" do
+    it "defaults to empty hash on bad json" do
       cs = CandidateSmasher.new("{}}")
-      expect(cs.valid?).to be(false)
-    end
-
-    it "requires @type property" do
-      cs = CandidateSmasher.new(no_type)
-      expect(cs.valid?).to be(false)
-    end
-
-    it "requires @type property is spek" do
-      cs = CandidateSmasher.new(not_spek)
-      expect(cs.valid?).to be(false)
-    end
-
-    it "checks for required attributes" do
-      cs = CandidateSmasher.new(missing_attr)
-      expect(cs.valid?).to be(false)
-    end
-
-    it "is valid when required attributes are present" do
-      cs = CandidateSmasher.new(full)
-      expect(cs.valid?).to be(true)
+      expect(cs.spek_hsh).to eq({})
     end
 
   end
 
-  describe "#generate_candidates" do
-    it "adds candidates to the spek" do
+  describe "#valid?" do
+    let(:content) do { "@type" => CandidateSmasher::SPEK_IRI,
+            CandidateSmasher::HAS_PERFORMER_IRI=> [],
+            CandidateSmasher::USES_TEMPLATE_IRI => [],
+            CandidateSmasher::USES_ISR_IRI => [] }
     end
 
-    it "adds candidates to the spek" do
+    subject do 
+      c = CandidateSmasher.new 
+      c.spek_hsh = content
+      c
     end
 
+    it "requires @type property" do
+      subject.spek_hsh.delete("@type")
+      expect(subject.valid?).to be(false)
+    end
+
+    it "requires @type property is spek" do
+      subject.spek_hsh["@type"] = "http://example.com/not/a/spek"
+      expect(subject.valid?).to be(false)
+    end
+
+    it "checks for required attributes" do
+      subject.spek_hsh.delete(CandidateSmasher::HAS_PERFORMER_IRI)
+      expect(subject.valid?).to be(false)
+    end
+
+    it "is valid when required attributes are present" do
+      expect(subject.valid?).to be(true)
+    end
+
+  end
+
+  describe "make_candidate" do
+    let(:performer) { {"@id" => "http://example.com/P1",
+                       "name" => "foo" } }
+    let(:template) { {"@id" => "http://example.com/T1",
+                       "colors" => "4" } }
+
+    it "assigns a random id" do
+      c = CandidateSmasher.make_candidate( template, performer )
+      expect(c["@id"]).to match(/^candidate.internal\/[a-f0-9]{32}$/)
+    end
+    
+    it "assigns the candidate type" do
+      c = CandidateSmasher.make_candidate( template, performer )
+      expect(c["@type"]).to eq(CandidateSmasher::CANDIDATE_IRI)
+    end
+
+    it "retains ancestor ids" do
+      c = CandidateSmasher.make_candidate( template, performer )
+      expect(c[CandidateSmasher::ANCESTOR_PERFORMER_IRI]).to eq(performer["@id"])
+      expect(c[CandidateSmasher::ANCESTOR_TEMPLATE_IRI]).to eq(template["@id"])
+    end
+
+  end
+
+  context "Aggregating Candidates" do
+    let(:content) do { "@type" => CandidateSmasher::SPEK_IRI,
+            CandidateSmasher::HAS_PERFORMER_IRI=> [
+              {"@id" => "http://example.com/P1"},
+              {"@id" => "http://example.com/P2"},
+              {"@id" => "http://example.com/P3"} ],
+            CandidateSmasher::USES_TEMPLATE_IRI => [
+              {"@id" => "http://example.com/T1"},
+              {"@id" => "http://example.com/T2"},
+              {"@id" => "http://example.com/T3"} ],
+            CandidateSmasher::USES_ISR_IRI => [] }
+    end
+
+    subject do 
+      c = CandidateSmasher.new 
+      c.spek_hsh = content
+      c
+    end
+
+    describe "#generate_all" do
+      it "returns (performers times templates) number of candidates " do
+        expect(subject.generate_candidates.length).to be(9)
+      end
+
+      it "returns candidates with unique ids" do
+        cands = subject.generate_candidates
+        expect(cands.length).to be(cands.uniq.length)
+      end
+    end
+
+    describe "#smash!" do
+      it "adds candidates to the spek" do
+        subject.smash!
+        expect(subject.spek_hsh.has_key?(CandidateSmasher::HAS_CANDIDATE_IRI)).to be(true)
+      end
+
+      it "is idempotent" do
+        subject.smash!
+        h1 = subject.spek_hsh.dup
+        subject.smash!
+        expect(subject.spek_hsh).to eq(h1)
+      end
+
+      it "returns a string" do
+        expect(subject.smash!.class).to be(String)
+      end
+
+      it "returns json" do
+        result = subject.smash!
+        expect{
+          JSON.parse(result)
+        }.not_to raise_error
+      end
+    end
   end
 end
